8a2c83a0a970d2e35b37cb395e70fe3cf8996c23
Itaxa84 <glebdolgoborodov@yandex.ru>
2023-08-05 23:32:45 +0300

    test

---
+++ Assets/Scripts/Gleb_scripts/DeadTriger.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadTriger : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        //var player = other.collider.GetComponent<Hel>
+    }
+}
